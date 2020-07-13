69c0002e66b0a8809482575843cc524304304ede
FreddyJR1995 <56648676+FreddyJR1995@users.noreply.github.com>
2020-07-12 23:47:12 -0500

    20-Resources Articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Http\Resources\Article as ArticleResource;
+use App\Http\Resources\ArticleCollection;
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Types\Collection;
 
 class ArticleController extends Controller
 {
     public function index() {
-        return Article::all();
+        return new ArticleCollection(Article::paginate());
     } public function show(Article  $article) {
-        return $article;
+        return new ArticleResource($article);
     } public function store(Request $request) {
         $article=Article::create($request->all());
         return response()->json($article, 201);
