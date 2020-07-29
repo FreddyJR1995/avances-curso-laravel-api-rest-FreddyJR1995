a132cd0ffa9902ff1a45038b77e36fc79e8552da
FreddyJR1995 <56648676+FreddyJR1995@users.noreply.github.com>
2020-07-29 13:07:48 -0500

    #28 Envio de correos

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Article;
 use App\Comment;
 use App\Http\Resources\Comment as CommentResource;
+use App\Mail\NewComment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class CommentController extends Controller
 {
@@ -32,7 +34,7 @@ class CommentController extends Controller
     public function show(Article $article, Comment $comment)
     {
         $comment = $article->comments()->where('id', $comment->id)->firstOrFail();
-        return response()->json($comment, 200);
+        return response()->json(new CommentResource($comment), 200);
     }
 
     /**
@@ -48,8 +50,11 @@ class CommentController extends Controller
             'text' => 'required|string'
         ]);
         $comment = $article->comments()->save(new Comment($request->all()));
-        return response()->json($comment,201);
+        Mail::to($article->user)->send(new NewComment($comment));
+        return response()->json(new CommentResource($comment), 201);
+        //return response()->json($comment, 201);
     }
+
     /**
      * Update the specified resource in storage.
      *
